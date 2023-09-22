23d3dbf9e16706d88c45f18fb43afa7386fb3a81
Ihor Kustov <zegorovskiy1355@gmail.com>
2023-09-22 18:20:24 +0300

    s

--- djangowebchat/asgi.py
+++ djangowebchat/asgi.py
@@ -13,7 +13,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            room.routing.websockett_urlpatterns
+            room.routing.websocket_urlpatterns
         )
     )
 })
